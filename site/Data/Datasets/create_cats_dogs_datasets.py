3672930b6f7ef1f019d7e6e30925ec4097de3cd8
Sergey Novichkov <NovichkovSD@yandex.ru>
2024-03-13 22:32:43 +0300

    Day 64 Update. Learned CNN for small datasets.

--- site/Data/Datasets/create_cats_dogs_datasets.py
+++ site/Data/Datasets/create_cats_dogs_datasets.py
@@ -1,17 +1,18 @@
 import os
 import shutil
 
-# The path to the directory where the original dataset was uncompressed
+# Путь к каталогу, в котором был разархивирован исходный набор данных с котами
 ORIGINAL_CAT_DATASET_DIR = f"{os.path.dirname(os.path.realpath(__file__))}/Kaggle-cats"
+# Путь к каталогу, в котором был разархивирован исходный набор данных с собаками
 ORIGINAL_DOG_DATASET_DIR = f"{os.path.dirname(os.path.realpath(__file__))}/Kaggle-dogs"
-# The directory where we will store our smaller dataset
+# Каталог, где мы будем хранить наш небольшой набор данных
 BASE_DIR = f"{os.path.dirname(os.path.realpath(__file__))}/cats_and_dogs_small"
 
 
 def main():
     os.mkdir(BASE_DIR)
 
-    # Directories for our training, validation and test splits
+    # Каталоги для наших наборов данных: обучение, валидация и тест
     train_dir = os.path.join(BASE_DIR, 'train')
     os.mkdir(train_dir)
     validation_dir = os.path.join(BASE_DIR, 'validation')
@@ -19,73 +20,73 @@ def main():
     test_dir = os.path.join(BASE_DIR, 'test')
     os.mkdir(test_dir)
 
-    # Directory with our training cat pictures
+    # Каталог с обучающими изображениями котов
     train_cats_dir = os.path.join(train_dir, 'cats')
     os.mkdir(train_cats_dir)
 
-    # Directory with our training dog pictures
+    # Каталог с обучающими изображениями собак
     train_dogs_dir = os.path.join(train_dir, 'dogs')
     os.mkdir(train_dogs_dir)
 
-    # Directory with our validation cat pictures
+    # Каталог с валидационными изображениями котов
     validation_cats_dir = os.path.join(validation_dir, 'cats')
     os.mkdir(validation_cats_dir)
 
-    # Directory with our validation dog pictures
+    # Каталог с валидационными изображениями собак
     validation_dogs_dir = os.path.join(validation_dir, 'dogs')
     os.mkdir(validation_dogs_dir)
 
-    # Directory with our validation cat pictures
+    # Каталог с тестовыми изображениями котов
     test_cats_dir = os.path.join(test_dir, 'cats')
     os.mkdir(test_cats_dir)
 
-    # Directory with our validation dog pictures
+    # Каталог с тестовыми изображениями собак
     test_dogs_dir = os.path.join(test_dir, 'dogs')
     os.mkdir(test_dogs_dir)
 
-    # Copy first 1000 cat images to train_cats_dir
+    # Копирование первых 1000 изображений с котами в каталог train_cats_dir
     fnames = ['{}.jpg'.format(i) for i in range(1000)]
     for fname in fnames:
         src = os.path.join(ORIGINAL_CAT_DATASET_DIR, fname)
         dst = os.path.join(train_cats_dir, fname)
         shutil.copyfile(src, dst)
 
-    # Copy next 500 cat images to validation_cats_dir
+    # Копирование следующих 500 изображений с котами в каталог validation_cats_dir
     fnames = ['{}.jpg'.format(i) for i in range(1000, 1500)]
     for fname in fnames:
         src = os.path.join(ORIGINAL_CAT_DATASET_DIR, fname)
         dst = os.path.join(validation_cats_dir, fname)
         shutil.copyfile(src, dst)
 
-    # Copy next 500 cat images to test_cats_dir
+    # Копирование следующих 500 изображений с котами в каталог test_cats_dir
     fnames = ['{}.jpg'.format(i) for i in range(1500, 2000)]
     for fname in fnames:
         src = os.path.join(ORIGINAL_CAT_DATASET_DIR, fname)
         dst = os.path.join(test_cats_dir, fname)
         shutil.copyfile(src, dst)
 
-    # Copy first 1000 dog images to train_dogs_dir
+    # Копирование первых 1000 изображений с собаками в каталог train_dogs_dir
     fnames = ['{}.jpg'.format(i) for i in range(1000)]
     for fname in fnames:
         src = os.path.join(ORIGINAL_DOG_DATASET_DIR, fname)
         dst = os.path.join(train_dogs_dir, fname)
         shutil.copyfile(src, dst)
 
-    # Copy next 500 dog images to validation_dogs_dir
+    # Копирование следующих 500 изображений с собаками в каталог validation_dogs_dir
     fnames = ['{}.jpg'.format(i) for i in range(1000, 1500)]
     for fname in fnames:
         src = os.path.join(ORIGINAL_DOG_DATASET_DIR, fname)
         dst = os.path.join(validation_dogs_dir, fname)
         shutil.copyfile(src, dst)
 
-    # Copy next 500 dog images to test_dogs_dir
+    # Копирование следующих 500 изображений с собаками в каталог test_dogs_dir
     fnames = ['{}.jpg'.format(i) for i in range(1500, 2000)]
     for fname in fnames:
         src = os.path.join(ORIGINAL_DOG_DATASET_DIR, fname)
         dst = os.path.join(test_dogs_dir, fname)
         shutil.copyfile(src, dst)
 
-    # Sanity checks
+    # Проверка корректности выполнения копирования
     print('total training cat images:', len(os.listdir(train_cats_dir)))
     print('total training dog images:', len(os.listdir(train_dogs_dir)))
     print('total validation cat images:', len(os.listdir(validation_cats_dir)))
